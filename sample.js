c5e966dea5a0bc7086d4e3b8e699251416d39cbb
Russ Posluszny <russjp1985@gmail.com>
2015-03-15 17:49:47 -0400

    A handful of updates
    * Use execSync.  It's not great, but it solves our problem.
    * Add docs to sample script and Executor

--- sample.js
+++ sample.js
@@ -1,76 +1,95 @@
-var File = require('./lib/File'),
-    Service = require('./lib/Service'),
+var Bundle = require('./lib/Bundle'),
+    Executor = require('./lib/Executor'),
+    File = require('./lib/File'),
     Package = require('./lib/Package'),
-    Bundle = require('./lib/Bundle'),
-    ResourceIndex = require('./lib/ResourceIndex'),
-    Executor = require('./lib/Executor');
+    Service = require('./lib/Service'),
+    _ = require('underscore'),
+    fs = require('fs');
 
-    // This could use any templating language you want!
-    template = function(opts) { return 'Hello world'; };
-
-var Apache = Bundle(function(apacheOpts) {
+// Create a bundle for Nginx
+// Bundles are a collection of tangible resources
+// In the case of Nginx, the tangible resources here are:
+//  * The package, which we want to ensure is installed
+//  * A configuration file, which we want to include
+//  * And a service, which we want to ensure is running and
+//  also reload if anything changes in this run
+var Nginx = Bundle(function(NginxOpts) {
 
     this.package = new Package({
-        name: 'apache2',
-        version: apacheOpts.version,
+        name: 'nginx',
+        version: NginxOpts.version,
     });
 
     this.file = new File({
-        path: '/etc/httpd/httpd.conf',
-        contents: template(apacheOpts),
+        path: '/etc/nginx/nginx.conf',
+        contents: fs.readFileSync(__dirname + '/global_nginx.conf'),
         dependsOn: [this.package],
     });
 
     this.service = new Service({
-        name: 'httpd',
+        name: 'nginx',
         dependsOn: [this.file, this.package]
     });
 });
 
 
-var apache = Apache({ title: 'Apache', version: '1.2.3.4' });
-
-// TODO: Allow pasing bundles as extra arguments
-var SiteA = Bundle(function(siteOpts) {
+// Create a bundle for a static Nginx site
+// Here, this bundle only has one resource -- a single configuration
+// file that we will install baed on how we iniitalize this bundle
+//
+// This bundle depends on Nginx, so you can see that we're expecting
+// an Nginx instance when we actually create the bundle instance for 
+// this static site.
+//
+// We're going to use a template to create this file so you can easily
+// add different variables for each instance, or perhpas though different
+// configurations.
+var staticSiteConf = fs.readFileSync(__dirname + '/site.conf').toString();
+var StaticSite = Bundle(function(siteOpts, nginxInstance) {
 
     this.site = new File({
-        path: '/etc/httpd/sites-enabled/site-a.conf'
+        path: '/etc/nginx/sites-enabled/' + siteOpts.configName + '.conf',
+        contents: _.template(staticSiteConf),
+        templateData: siteOpts
     });
-    this.site.dependsOn(apache);
-});
-
-// Also support syntax where dependencies are passed into
-// the bundle
-var SiteB = Bundle(function(siteOpts, apacheInstance) {
-    this.site = new File({
-        path: '/etc/httpd/sites-enabled/site-a.conf'
-    });
-    this.site.dependsOn(apacheInstance);
-});
-
-var Host = Bundle(function(hostOpts) {
-    this.siteA = SiteA({ title: 'SiteA' });
-    this.siteB = SiteB({ title: 'SiteB' }, apache);
+    this.site.dependsOn(nginxInstance);
 });
 
 
-var host = Host({ title: 'web2', one: 2, two: 3 });
+// OK. Now let's configure a simple web host.
+// We don't really have the concept of Hosts / Roles, etc.
+// Instead, we just bundle things. This host will bundle 2
+// StaticSites 
+var Web = Bundle(function(hostOpts) {
+    // Create an instance of Nginx
+    this.nginx = Nginx({ version: '1.1.19-1ubuntu0.7' });
 
-var order = Executor.orderDependenciesForObject(host);
-var catalog = ResourceIndex.getCatalog();
+    this.siteA = StaticSite({
+        port: 8000,
+        server_name: hostOpts.name,
+        configName: 'site-a',
+        webroot: '/var/www'
+    }, this.nginx);
 
-var os = {
-    sh: {
-        run:  function(cmd) { console.log(">>>: " + cmd); return 0; },
-        exec: function(cmd) { console.log(">>>: " + cmd); return 0; },
-    },
+    this.siteB = StaticSite({
+        port: 9000,
+        server_name: hostOpts.altName,
+        configName: 'site-b',
+        webroot: '/var/www'
+    }, this.nginx);
+});
 
-    rm: function(path) { console.log(">>>: rm " + path); },
-    link: function(src, dest) { console.log(">>>: ln -s " + src + ' ' + dest); },
-    writeFile: function(path, contents) { console.log(">>>: cat $FILE > " + path); },
-    chmod: function(path, perms) { console.log(">>>: chmod "+path+" "+perms); },
-    chown: function(path, owner, group) { console.log(">>>: chown " + path); },
-};
 
-var hostInfo = { hostname: 'hello world' };
-Executor.processQueue(order, ResourceIndex, os, hostInfo);
+// Create a host and provision it
+var host = Web({
+    name: 'mysite.com',
+    altName: 'static.mysite.com',
+});
+
+
+// Provision the host.
+// Provide any extra data here.  This could be useful for production / development
+// configs, etc.
+var extraData = {};
+
+Executor.provision(host, extraData);
